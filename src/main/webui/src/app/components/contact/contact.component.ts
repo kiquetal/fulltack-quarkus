f73cf1995e4b224ca919d9c29b1216bb7ac9d9b8
kiquetal <kiquetal@gmail.com>
2024-07-21 13:12:43 -0400

    Updating css

--- src/main/webui/src/app/components/contact/contact.component.ts
+++ src/main/webui/src/app/components/contact/contact.component.ts
@@ -1,12 +1,15 @@
 import {Component, OnInit} from '@angular/core';
 import {BackendService} from "../../services/backend.service";
 import {Observer, Subscription} from "rxjs";
+import {NgIf} from "@angular/common";
 
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
@@ -14,7 +17,8 @@ export class ContactComponent implements OnInit {
  result: string = " ";
 
   constructor(private httpService : BackendService) {  }
-
+  hasError: boolean = false;
+  errorMessage: string = " ";
   ngOnInit(): void {
     this.httpService.fetchData("/something").subscribe(this.observerData());
 
@@ -28,7 +32,11 @@ export class ContactComponent implements OnInit {
         console.log("Value",value);
         this.result = value;
       },
-      error: (error) => console.log("Found Error!!!",error)
+      error: (error) => {
+        console.log("Found Error!!!",error)
+        this.hasError = true;
+        this.errorMessage = JSON.stringify(error.status);
+      }
     }
 
   }
