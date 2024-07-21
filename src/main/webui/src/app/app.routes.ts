cab074c2ed3c2ed320bc8f3b75165b4501232359
kiquetal <kiquetal@gmail.com>
2024-07-21 09:57:19 -0400

    Moving files, and creating service

--- src/main/webui/src/app/app.routes.ts
+++ src/main/webui/src/app/app.routes.ts
@@ -1,8 +1,7 @@
 import { Routes } from '@angular/router';
-import {AppComponent} from "./app.component";
-import {HomeComponent} from "../components/home/home.component";
-import {AboutComponent} from "../components/about/about.component";
-import {ContactComponent} from "../components/contact/contact.component";
+import {HomeComponent} from "./components/home/home.component";
+import {AboutComponent} from "./components/about/about.component";
+import {ContactComponent} from "./components/contact/contact.component";
 
 export const routes: Routes = [
   {
