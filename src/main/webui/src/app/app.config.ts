69a7f270df2746920bbb34f37ba00aafbad5a0da
kiquetal <kiquetal@gmail.com>
2024-07-21 10:06:49 -0400

    Sending new invocation

--- src/main/webui/src/app/app.config.ts
+++ src/main/webui/src/app/app.config.ts
@@ -6,10 +6,12 @@ import { routes } from './app.routes';
 import {provideAnimations} from "@angular/platform-browser/animations";
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import {MatSidenavModule} from "@angular/material/sidenav";
+import {provideHttpClient} from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes,withHashLocation()),
    provideAnimations(),
+    provideHttpClient(),
   importProvidersFrom(MatSidenavModule,MatListModule), provideAnimationsAsync()]
 };
--- src/main/webui/src/app/components/contact/contact.component.ts
+++ src/main/webui/src/app/components/contact/contact.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BackendService} from "../../services/backend.service";
+import {Observer, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-contact',
@@ -7,6 +9,27 @@ import { Component } from '@angular/core';
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+
+  constructor(private httpSerice : BackendService) {  }
+
+  ngOnInit(): void {
+    this.httpSerice.obtainResponse("/hello").subscribe(this.observerData());
+
+  }
+
+  private observerData(): Observer<any>{
+
+    return {
+      next: (value) => console.log(value),
+      error: (error) => console.log(error),
+      complete: () => console.log("completed  ")
+    }
+
+  }
+
+
+
+
 
 }
