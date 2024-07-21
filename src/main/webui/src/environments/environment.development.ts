750c2f1b0c16a2a44b6b60e9baa2c7c9b5442eec
kiquetal <kiquetal@gmail.com>
2024-07-20 21:54:27 -0400

    using angular

---
+++ src/main/webui/src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  url:{
+    endpoint: "http://localhost:8080"	  
+  }
+
+};
+
---
+++ src/main/webui/src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+	production: true,
+	url:{
+	endpoint:"http://localhost:8080"
+	}
+
+};
