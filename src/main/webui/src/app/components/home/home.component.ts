b6c6f89d505c4d865587e55c6c0decd93c8cf84b
kiquetal <kiquetal@gmail.com>
2024-07-24 17:43:41 -0400

    Changing a lot of the html

--- src/main/webui/src/app/components/home/home.component.ts
+++ src/main/webui/src/app/components/home/home.component.ts
@@ -1,9 +1,28 @@
 import { Component } from '@angular/core';
 
+import {
+  MatCard,
+  MatCardActions,
+  MatCardContent,
+  MatCardHeader,
+  MatCardSubtitle,
+  MatCardTitle
+} from "@angular/material/card";
+import {MatChip,  MatChipsModule} from "@angular/material/chips";
+
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardTitle,
+    MatCardSubtitle,
+    MatCardHeader,
+    MatChipsModule,
+    MatCardContent,
+    MatCardActions,
+    MatChip,
+    MatCard
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
