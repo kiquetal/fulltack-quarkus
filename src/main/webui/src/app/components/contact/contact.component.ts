741672418bce8c78e101d2d9ba810e6068359ec2
kiquetal <kiquetal@gmail.com>
2024-07-24 09:02:04 -0400

    Fix some html to be able to add transition to collapsible

--- src/main/webui/src/app/components/contact/contact.component.ts
+++ src/main/webui/src/app/components/contact/contact.component.ts
@@ -3,7 +3,18 @@ import {BackendService} from "../../services/backend.service";
 import {Observer} from "rxjs";
 import {NgForOf, NgIf} from "@angular/common";
 import {MatList, MatListItem} from "@angular/material/list";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
+
+class ProjectRepresentation {
+  name: string;
+  isVisible: boolean;
+
+  constructor(name: string, isVisible: boolean) {
+    this.name = name;
+    this.isVisible = isVisible;
+  }
+}
 
 @Component({
   selector: 'app-contact',
@@ -16,7 +27,24 @@ import {MatList, MatListItem} from "@angular/material/list";
   ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css',
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  animations:[
+    trigger('detailAnimation',[
+      state('hidden',style({
+        opacity: 0,
+        height: 0,
+        overflow: 'hidden'
+
+      })  ),
+      state('visible',style({
+        opacity: 1,
+        height: '*',
+        overflow: 'visible'
+      })  ),
+     transition('hidden => visible', [animate('500ms ease-in')]),
+     transition('visible => hidden', [animate('500ms ease-out')
+       ]  )
+  ] )  ]
 })
 export class ContactComponent implements OnInit {
  result: string = " ";
@@ -24,7 +52,7 @@ export class ContactComponent implements OnInit {
   constructor(private httpService : BackendService) {  }
   hasError: boolean = false;
   errorMessage: string = " ";
-  projects: string[] = [];
+  projects: ProjectRepresentation[] = [];
   ngOnInit(): void {
     this.httpService.fetchData("/api/projects").subscribe(this.observerData());
 
@@ -39,8 +67,12 @@ export class ContactComponent implements OnInit {
        console.log("Value-from-server",value);
 //       const correctedValue = value.replace(/'/g, '"');
   //     this.projects = JSON.parse(correctedValue)["projects"];
-      this.projects = JSON.parse(value)["projects"];
-       console.log("Projects",this.projects);
+      const originalProjects  = JSON.parse(value)["projects"];
+
+      for (let project of originalProjects){
+        this.projects.push(new ProjectRepresentation(project, false));
+      }
+
       },
       error: (error) => {
         console.log("Found Error!!!",error)
